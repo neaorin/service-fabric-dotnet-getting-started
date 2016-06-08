a9087077c7b44826b1c175103a9171a68d049b3a
neaorin <neaorin@gmail.com>
2016-06-08 10:02:49 +0200

    Trivia Question Stub

---
+++ Services/Chatter/Chat.Domain/TriviaDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Domain
+{
+    public class TriviaDatabase
+    {
+        public static KeyValuePair<string, string> GetRandomQuestion()
+        {
+            return new KeyValuePair<string, string>(
+                "What is the answer to the Ultimate Question of Life, The Universe, and Everything?",
+                "42"
+                );
+
+        }
+    }
+}
